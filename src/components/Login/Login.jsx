c6d8d5f40519e9abbae72948a03c66fc54f09fb8
Alejandro <alejandrom.am33@gmail.com>
2022-05-08 01:40:54 -0500

    change: icon and title of the site

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Form from './Form';
+import DocumentTitle from 'react-document-title';
 
 export default function Login() {
     return (
         <div>
+            <DocumentTitle title="devJobs | Login"/>
             <div className='logo-form'>
                 <h1 className='logo'>devJobs<span className='logo-span'>()</span></h1>
             </div>
--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../Navbar';
 import Filters from './Filters';
+import DocumentTitle from 'react-document-title';
 
 export default function Feed() {
     return (
         <>
+            <DocumentTitle title="devJobs | Feed"/>
             <Navbar/>
             <Filters/>
         </>
