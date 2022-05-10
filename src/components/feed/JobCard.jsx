9b4ecb905107ccc450121ddcd738a00ba96ba390
Alejandro <alejandrom.am33@gmail.com>
2022-05-10 13:41:02 -0500

    add: dynamism to the job cards and the info into them

--- src/components/feed/JobCard.jsx
+++ src/components/feed/JobCard.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookmark } from '@fortawesome/free-regular-svg-icons'
 import companyImgDefault from '../../assets/company.svg'
 
-export default function JobCard() {
+export default function JobCard({job}) {
 
     const profile = <FontAwesomeIcon className='fav-icon' icon={ faBookmark } />
 
@@ -12,75 +12,16 @@ export default function JobCard() {
             <div className='job-card'>
                 <div className="job-img-container">
                     <img className='job-img' src={ companyImgDefault }
-                    srcSet=""
+                    srcSet={ job.companyImg }
                     alt="Img empresa"/>
                 </div>
                 <div className='job-info-container'>
-                    <h3 className='job-title'>Front End Developer</h3>
-                    <p className='job-desc'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quas, quidem minima corporis aliquam quae fugiat culpa architecto rerum quos ullam qui numquam, nobis cupiditate omnis tenetur. Voluptatum, sequi quod.</p>    
+                    <h3 className='job-title'>{`${job.company}   |   ${ job.title }`}</h3>
+                    <p className='job-desc'>{ job.description }</p>    
                 </div>
                 { profile }
             </div>
-            <div className='job-card'>
-            <div className="job-img-container">
-                <img className='job-img' src={ companyImgDefault }
-                srcSet=""
-                alt="Img empresa"/>
-            </div>
-            <div className='job-info-container'>
-                <h3 className='job-title'>Front End Developer</h3>
-                <p className='job-desc'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quas, quidem minima corporis aliquam quae fugiat culpa architecto rerum quos ullam qui numquam, nobis cupiditate omnis tenetur. Voluptatum, sequi quod.</p>    
-            </div>
-            { profile }
-            </div>
-            <div className='job-card'>
-                <div className="job-img-container">
-                    <img className='job-img' src={ companyImgDefault }
-                    srcSet=""
-                    alt="Img empresa"/>
-                </div>
-                <div className='job-info-container'>
-                    <h3 className='job-title'>Front End Developer</h3>
-                    <p className='job-desc'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quas, quidem minima corporis aliquam quae fugiat culpa architecto rerum quos ullam qui numquam, nobis cupiditate omnis tenetur. Voluptatum, sequi quod.</p>    
-                </div>
-                { profile }
-            </div>
-            <div className='job-card'>
-                <div className="job-img-container">
-                    <img className='job-img' src={ companyImgDefault }
-                    srcSet=""
-                    alt="Img empresa"/>
-                </div>
-                <div className='job-info-container'>
-                    <h3 className='job-title'>Front End Developer</h3>
-                    <p className='job-desc'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quas, quidem minima corporis aliquam quae fugiat culpa architecto rerum quos ullam qui numquam, nobis cupiditate omnis tenetur. Voluptatum, sequi quod.</p>    
-                </div>
-                { profile }
-            </div>
-            <div className='job-card'>
-                <div className="job-img-container">
-                    <img className='job-img' src={ companyImgDefault }
-                    srcSet=""
-                    alt="Img empresa"/>
-                </div>
-                <div className='job-info-container'>
-                    <h3 className='job-title'>Front End Developer</h3>
-                    <p className='job-desc'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quas, quidem minima corporis aliquam quae fugiat culpa architecto rerum quos ullam qui numquam, nobis cupiditate omnis tenetur. Voluptatum, sequi quod.</p>    
-                </div>
-                { profile }
-            </div>
-            <div className='job-card'>
-                <div className="job-img-container">
-                    <img className='job-img' src={ companyImgDefault }
-                    srcSet=""
-                    alt="Img empresa"/>
-                </div>
-                <div className='job-info-container'>
-                    <h3 className='job-title'>Front End Developer</h3>
-                    <p className='job-desc'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quas, quidem minima corporis aliquam quae fugiat culpa architecto rerum quos ullam qui numquam, nobis cupiditate omnis tenetur. Voluptatum, sequi quod.</p>    
-                </div>
-                { profile }
-            </div>
+
         </>
         
     )
--- src/components/feed/Main.jsx
+++ src/components/feed/Main.jsx
@@ -2,13 +2,46 @@ import React from 'react'
 import Aside from './Aside'
 import JobCard from './JobCard'
 
+const jobs = [
+    {
+        id: 1,
+        title: 'Front End Developer',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quas, quidem minima corporis aliquam quae fugiat culpa architecto rerum quos ullam qui numquam, nobis cupiditate omnis tenetur. Voluptatum, sequi quod.',
+        company: 'Google',
+        companyImg: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+    },
+    {
+        id: 2,
+        title: 'Back End Developer',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quas, quidem minima corporis aliquam quae fugiat culpa architecto rerum quos ullam qui numquam, nobis cupiditate omnis tenetur. Voluptatum, sequi quod.',
+        company: 'Amazon',
+        companyImg: 'https://guiaimpresion.com/wp-content/uploads/2020/06/Logotipo-Amazon.jpg',
+    },
+    {
+        id: 3,
+        title: 'Full Stack Developer',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quas, quidem minima corporis aliquam quae fugiat culpa architecto rerum quos ullam qui numquam, nobis cupiditate omnis tenetur. Voluptatum, sequi quod.',
+        company: 'Meta',
+        companyImg: 'https://logos-marcas.com/wp-content/uploads/2021/10/Meta-facebook-Nuevo-Logotipo.jpg',
+    },
+    {
+        id: 4,
+        title: 'Software Engineer',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quas, quidem minima corporis aliquam quae fugiat culpa architecto rerum quos ullam qui numquam, nobis cupiditate omnis tenetur. Voluptatum, sequi quod.',
+        company: 'Twitter',
+        companyImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/1280px-Twitter-logo.svg.png',
+    }
+]
+
 export default function Main() {
     return (
         <div className='grid-main container'>
             <main>
                 <h2>6 jobs found</h2>
                 <div className="jobs-container">
-                    <JobCard/>
+                    {jobs.map(job=>(
+                    <JobCard key={job.id} job={job}/>
+                    ))}
                 </div>
             </main>
             <Aside/>
