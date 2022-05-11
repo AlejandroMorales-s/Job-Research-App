c5dbd2159bc0064d0d5311f0c47fd7b28d6cf941
Alejandro <alejandrom.am33@gmail.com>
2022-05-10 19:41:49 -0500

    add: extra info in job cards and funcionality of favorites

--- src/components/feed/JobCard.jsx
+++ src/components/feed/JobCard.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBookmark } from '@fortawesome/free-regular-svg-icons'
 import companyImgDefault from '../../assets/company.svg'
+import { FavoritesContext } from './Main'
 
 export default function JobCard({job}) {
+    
+    const {favorites,setFavorites} = useContext(FavoritesContext);
+    
+    const addToFavorites = () =>{
+        if (favorites.find(fav => fav.id === job.id)){
+            const newFavorites = favorites.filter(fav => fav.id !== job.id);
+            setFavorites(newFavorites);
+        } else {
+            setFavorites([...favorites,job]);
+        }
+    }
 
-    const profile = <FontAwesomeIcon className='fav-icon' icon={ faBookmark } />
+    
+    const fav = <FontAwesomeIcon onClick={addToFavorites} className='fav-icon' icon={ faBookmark } />
 
     return (
         <>
@@ -18,8 +31,14 @@ export default function JobCard({job}) {
                 <div className='job-info-container'>
                     <h3 className='job-title'>{`${job.company}   |   ${ job.title }`}</h3>
                     <p className='job-desc'>{ job.description }</p>    
+                    <div className="job-extrainfo-container">
+                        <p className="extrainfo">{`Seniority: ${job.seniority}`}</p>
+                        <p className="extrainfo">{`Salary: ${job.salary}`}</p>
+                        <p className="extrainfo">{`Languaje: ${job.languages}`}</p>
+                        <p className="extrainfo">{`Experience: ${job.experience}`}</p>
+                    </div>
                 </div>
-                { profile }
+                { fav }
             </div>
 
         </>
--- src/components/feed/Main.jsx
+++ src/components/feed/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, createContext} from 'react'
 import Aside from './Aside'
 import JobCard from './JobCard'
 
@@ -8,7 +8,11 @@ const jobs = [
         title: 'Front End Developer',
         description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quas, quidem minima corporis aliquam quae fugiat culpa architecto rerum quos ullam qui numquam, nobis cupiditate omnis tenetur. Voluptatum, sequi quod.',
         company: 'Google',
-        companyImg: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+        companyImg: 'https://e7.pngegg.com/pngimages/543/934/png-clipart-google-app-logo-google-logo-g-suite-google-text-logo.png',
+        salary: '$100,000',
+        seniority: 'Junior',
+        languages: 'JavaScript, React, Redux, NodeJS',
+        experience: '1 year',
     },
     {
         id: 2,
@@ -16,6 +20,10 @@ const jobs = [
         description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quas, quidem minima corporis aliquam quae fugiat culpa architecto rerum quos ullam qui numquam, nobis cupiditate omnis tenetur. Voluptatum, sequi quod.',
         company: 'Amazon',
         companyImg: 'https://guiaimpresion.com/wp-content/uploads/2020/06/Logotipo-Amazon.jpg',
+        salary: '$100,000',
+        seniority: 'Trainee',
+        languages: 'JavaScript, React, Redux, NodeJS',
+        experience: '1 year'
     },
     {
         id: 3,
@@ -23,6 +31,10 @@ const jobs = [
         description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quas, quidem minima corporis aliquam quae fugiat culpa architecto rerum quos ullam qui numquam, nobis cupiditate omnis tenetur. Voluptatum, sequi quod.',
         company: 'Meta',
         companyImg: 'https://logos-marcas.com/wp-content/uploads/2021/10/Meta-facebook-Nuevo-Logotipo.jpg',
+        salary: '$100,000',
+        seniority: 'Mid',
+        languages: 'JavaScript, React, Redux, NodeJS',
+        experience: '1 year'
     },
     {
         id: 4,
@@ -30,21 +42,37 @@ const jobs = [
         description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias quas, quidem minima corporis aliquam quae fugiat culpa architecto rerum quos ullam qui numquam, nobis cupiditate omnis tenetur. Voluptatum, sequi quod.',
         company: 'Twitter',
         companyImg: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/1280px-Twitter-logo.svg.png',
+        salary: '$100,000',
+        seniority: 'Senior',
+        languages: 'JavaScript, React, Redux, NodeJS',
+        experience: '1 year'
     }
 ]
 
+export const FavoritesContext = createContext()
+
+
 export default function Main() {
+
+    const [favorites, setFavorites] = useState([])
+    
     return (
-        <div className='grid-main container'>
-            <main>
-                <h2>6 jobs found</h2>
-                <div className="jobs-container">
-                    {jobs.map(job=>(
-                    <JobCard key={job.id} job={job}/>
-                    ))}
-                </div>
-            </main>
-            <Aside/>
-        </div>
+        <FavoritesContext.Provider value={{
+            favorites,
+            setFavorites
+            
+        }}>
+            <div className='grid-main container'>
+                <main>
+                    <h2>{`${jobs.length} jobs found`}</h2>
+                    <div className="jobs-container">
+                        {jobs.map(job=>(
+                        <JobCard key={job.id} job={job}/>
+                        ))}
+                    </div>
+                </main>
+                <Aside/>
+            </div>
+        </FavoritesContext.Provider>
     )
 }
