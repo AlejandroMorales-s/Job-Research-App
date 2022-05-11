c1bef6093db59ca8f048bebffcbb90c0af0d2d08
Alejandro <alejandrom.am33@gmail.com>
2022-05-11 00:36:49 -0500

    add: favorites section

--- src/components/favorites/Favorites.jsx
+++ src/components/favorites/Favorites.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { FavoritesContext } from '../../App'
 import Navbar from '../Navbar'
 import DocumentTitle from 'react-document-title';
+import JobCard from '../feed/JobCard';
+import Empty from './Empty';
 
 export default function Favorites() {
+    
+    const {favorites,setFavorites} = useContext(FavoritesContext);
+
     return (
         <>
             <DocumentTitle title="devJobs | Favorites"/>
             <Navbar/>
+            {favorites.length === 0 ? (
+                <Empty/>
+                ) : (
+                    <>
+                        <h2 className='favorites-title align-text'>Favorites</h2>
+                        <div className="jobs-container favorites-container">
+                            {favorites.map(job=>(
+                            <JobCard key={job.id} job={job}/>
+                            ))}
+                        </div>
+                    </>
+                )}
         </>
     )
 }
