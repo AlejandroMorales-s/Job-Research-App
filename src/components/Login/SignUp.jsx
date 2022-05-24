9fc5c131b2b4c82fa7a47601b3e6f11a9b3f9f1a
Alejandro <alejandrom.am33@gmail.com>
2022-05-23 19:38:37 -0500

    add: regex to the password

--- src/components/Login/SignUp.jsx
+++ src/components/Login/SignUp.jsx
@@ -1,15 +1,18 @@
-import React, {useRef, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import { post } from '../../api'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheck, faX } from '@fortawesome/free-solid-svg-icons'
 
 export default function SignUp() {
     const [error,setError] = useState({
         isError:false,
         message:"",
-    })
-    const email = useRef()
-    const password = useRef()
-    const name = useRef()
-    const role = useRef()
+    });
+    const [valid, setValid] = useState(false);
+    const email = useRef();
+    const password = useRef();
+    const name = useRef();
+    const role = useRef();
 
     const signup = (event) =>{
         event.preventDefault()
@@ -24,7 +27,6 @@ export default function SignUp() {
             alert("User created successfully")
         })
         .catch(error=>{
-            console.log(error.response.data)
             setError({
                 isError:true,
                 message:error.response.data.message,
@@ -33,7 +35,20 @@ export default function SignUp() {
         })
     }
 
+    const regex = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/
 
+    const validation = () => {
+        const pass = password.current.value
+
+        regex.test(pass) ? setValid(true) : setValid(false)
+    }
+
+    useEffect(()=>{
+        password.current.oninput = validation
+    },[password])
+
+    const check = <FontAwesomeIcon className='check' icon={ faCheck } />
+    const fail = <FontAwesomeIcon className='fail' icon={ faX } />
 
     return (
     <>
@@ -57,6 +72,13 @@ export default function SignUp() {
             <div className='campos-container'>
                 <label className='form-label' htmlFor="password">password</label>
                 <input ref={password} id='password' className='form-input' type="password" placeholder="password" />
+                <div>
+                    {valid ? 
+                        <p className='password-check'>{check} Password valid!</p>
+                    : 
+                        <p className='password-fail'>{fail} 8-16 characters, at least one digit, one lower case letter and one upper case letter.</p>
+                    }
+                </div>
                 <button className='form-link form-btn' type="submit">Sign Up</button>
             </div>
         </form>
