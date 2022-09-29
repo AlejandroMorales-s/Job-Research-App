0e6c744c11f456643aaa2596f193fdb635b7d3e3
Alejandro <alejandrom.am33@gmail.com>
2022-09-29 02:16:45 -0500

    remove: recover of the session with old backend

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,59 +1,14 @@
-import React, { useState, useContext } from 'react';
+import React, { useState} from 'react';
 import Form from './Form';
 import DocumentTitle from 'react-document-title';
 import SignUp from './SignUp';
-import {useNavigate} from 'react-router-dom';
-import { GlobalContext } from '../../App';
 
 export default function Login() {
     const [form, setForm] = useState(true);
-    const {auth, setAuth} = useContext(GlobalContext)
-    const navigate = useNavigate();
 
     const toggle = () => {
         setForm(!form);
     }
-
-    const recoverSession = ()=>{
-        const token = localStorage.getItem("token")
-    
-        if(token){
-            fetch("https://backendnodejstzuzulcode.uw.r.appspot.com/api/auth/validate",{
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json",
-                "Authorization":"Bearer "+ token
-            },
-            })
-            .then(res=>res.json())
-            .then(data=>{
-                if(data.failed){
-                    console.log(data)
-                }else{
-                    setAuth({
-                        id:data.user.id,
-                        name:data.user.name,
-                        email:data.user.email,
-                        role:data.user.role,
-                        logged:true
-                    })
-
-                    if (data.user.role === "applicant") {
-                        navigate("/feed",{
-                            replace:true
-                        })
-                    } else {
-                        navigate("/myoffers",{
-                            replace:true
-                        })
-                    }
-                }
-            })
-            .catch(error=>console.log(error))
-        }    
-    }
-
-    recoverSession()
 
     return (
         <div>
